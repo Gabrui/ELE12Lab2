bd43311590a7d815960323654d96079a0ec3f76f
dpcprince <dpcprince@gmail.com>
2017-06-22 01:50:47 -0300

    Add files via upload
    Cálculo de w aprimorado para retornar sempre o menor erro

--- codigo/output.js
+++ codigo/output.js
@@ -1,14 +1,91 @@
-function wd(a,b,er){
-    
-    var resultado;
-    resultado = 8*Math.exp(a)/(Math.exp(2*a)-2);
-    if (resultado > 2){
-        
-        resultado = 2/Math.pi*(b-1-Math.log(2*b-1)+(er-1)/(2*er)*(Math.log(b-1)+0.39-0.61/er));
+/**
+ * @function [beta] Calcula o coeficiente dieletrico efetivo
+ * @constant {number} k0 numero de onda padrao
+ * @param {number} er constante dieletrica relativa
+ * @constant {number} c velocidade da luz
+ * @param {number} f frequencia
+ * @returns constante de propagacao
+ */
+function beta(f, er) {
+    c = 300000000;
+    k0 = 2 * Math.pi * f / c;
+    return k0 * Math.sqrt(er);
+}
+
+/**
+ * @function [vp] Calcula a velocidade de fase em metros por segundo
+ * @param {number} ee coeficiente dieletrico efetivo
+ * @constant {number} c velocidade da luz
+ * @returns velocidade de fase
+ */
+function vp(ee) {
+    c = 300000000;
+    return c / Math.sqrt(ee);
+}
+
+/**
+ * Calcula o coeficiente dieletrico efetivo
+ * @param {number} w espessura da fita condutora
+ * @param {number} d espessura do dieletrico
+ * @param {number} er constante dieletrica relativa
+ * @returns coeficiente dieletrico efetivo
+ */
+function cde(er, d, w) {
+
+    return (er + 1) / 2 + ((er - 1) / 2) * (1 / Math.sqrt(1 + 12 * d / w));
+}
+
+/**
+ * Calcula a impedancia intrinseca
+ * @param {number} ee dieletrico efetivo
+ * @param {number} w espessura da fita condutora
+ * @param {number} d espessura do dieletrico
+ * @returns {number} impedancia intrinseca ohm
+ */
+function Z0(d, w, ee) {
+
+    var wd = w / d;
+    if (wd <= 1) {
+
+        z0 = 60 / Math.sqrt(ee) * Math.log(8 * d / w + w / (4 * d));
     }
+    else {
+
+        z0 = 120 * Math.PI / (Math.sqrt(ee) * (w / d + 1.393 + 0.667 * Math.log(w / d + 1.444)));
+    }
+    return z0;
+
+}
+
+
+/**
+ * Calcula a razao w por d
+ * @param {number} er dieletrico relativo
+ * @param {number} b constatne numerica
+ * @param {number} a constante numerica
+ * @returns {number} wd razao da largura da fita pela espessura do dieletrico
+ */
+function wd(a, b, er, d, Z) {
     
+    var resultado, resultado2, erro, erro2;
+
+    resultado = 2 / Math.PI * (b - 1 - Math.log(2 * b - 1) + (er - 1) / (2 * er) * (Math.log(b - 1) + 0.39 - 0.61 / er));
+
+    resultado2 = 8 * Math.exp(a) / (Math.exp(2 * a) - 2);
+
+    var ee;
+
+    ee = cde(er, d, d * resultado);
+    erro = Math.pow(Z - Z0(d, d * resultado, ee), 2);
+    ee = cde(er, d, d * resultado2);
+    erro2 = Math.pow(Z - Z0(d, d * resultado2, ee), 2);
+
+    if (erro2 < erro) {
+        resultado = resultado2;
+    }
     return resultado;
 }
+
 /**
  * Calcula a constante numerica A
  * @param {number} er dieletrico relativo
@@ -19,6 +96,7 @@ function A(z0,er){
    
     return (z0/60)*Math.sqrt((er+1)/2)+ ((er-1)/(er+1))*(0.23+0.11/er);
 }
+
 /**
  * Calcula a constante numerica B
  * @param {number} er dieletrico relativo
@@ -31,26 +109,32 @@ function B(z0,er){
     
 }
 
-function cde(er,d,w){
-    
-    return (er+1)/2 + ((er-1)/2)*(1/Math.sqrt(1+12*d/w));
-}
 
-function Z0(d,w,ee){
-    
-    var wd = w/d;
-    if (wd<= 1){
-        
-        z0 = 60/Math.sqrt(ee)*Math.log(8*d/w + w/(4*d));
-    }
-    else{
-        
-        z0 = 120*Math.PI/(Math.sqrt(ee)*(w/d+1.393+0.667*Math.log(w/d+1.444)));
-    }
-    return z0;
-    
-}
+/**
+ * Calcula a atenuacao no dieletrico
+ * @param {number} k0 numero de onda padrao
+ * @param {number} tdelta tangente de perdas
+ * @param {number} ee dieletrico efetivo
+ * @param {number} er dieletrico relativo
+ * @returns {number} atenuacao no dieletrico Np/m
+ */
+function alfad(k0, er, ee, tdelta) {
 
+    return k0 * er * (ee - 1) * tdelta / (2 * Math.sqrt(ee) * (er - 1));
+
+}
+/**
+ * 
+ * @param {number} w largura da fita condutora
+ * @param {number} z0 impedancia intrinseca
+ * @param {number} rs resistencia superficial ou de folha
+ * @returns {number} atenuacao no condutor Np/m
+ */
+function alfac(rs, z0, w) {
+
+    return rs / (z0 * w);
+
+}
 
 $( document ).ready(function() {
 
@@ -79,7 +163,8 @@ $( document ).ready(function() {
     	$('#analisar_title').text('Input')
     });
 
-    $(".btn-success").on("click",function(){
+    $(".btn-success").on("click", function () {
+
     	if(design)
     	{
     	var $inputs = $('#entradas1 :input');
@@ -103,13 +188,11 @@ $( document ).ready(function() {
 		
     	var a = A(z0,epsilon);
     	var b = B(z0,epsilon);
-
-    	var W = altura*wd(a,b,epsilon);
+    	var W = altura*wd(a,b,epsilon, altura, z0);
     	var cd_eff = cde(epsilon,altura,W);
-    	var L = (theta/360)*(300/frequencia)/Math.sqrt(cd_eff);
+    	var L = (theta / 360) * (300 / frequencia) / Math.sqrt(cd_eff);
     	$('#W').val(W);
     	$('#L').val(L);
-   
 		audio1.play();
     	}
     	else if(analisar)
@@ -142,12 +225,6 @@ $( document ).ready(function() {
     	}
 
 
-
     });
-
-
-
-
-
 
 });
